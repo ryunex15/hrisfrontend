1f7be87a2860c6110744712c24402757717ddff4
ryunex15 <badarudin1511@gmail.com>
2024-07-08 11:10:24 +0700

    tambah button

--- app/datapengguna/page.jsx
+++ app/datapengguna/page.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 
 const DataPengguna = () => {
-   return <div className="bg-white rounded-lg mx-4 p-4">Data Pengguna</div>;
+   return <div className="bg-white rounded-lg p-4 mb-10 mt-4">Data Pengguna
+   <br />
+   <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2.5 me-2 mb-2 mt-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Tambah Data</button>
+   </div>;
 };
-
-
-
-
 
 export default DataPengguna;
