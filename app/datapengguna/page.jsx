fb46540ca72ef7f93ce471208e394c9e8b1a0a45
ryunex15 <badarudin1511@gmail.com>
2024-07-08 11:13:23 +0700

    posisi

--- app/datapengguna/page.jsx
+++ app/datapengguna/page.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 const DataPengguna = () => {
-   return <div className="bg-white rounded-lg p-4 mb-10 mt-4">Data Pengguna
+   return <div className="bg-white rounded-lg p-4 text-xl">Data Pengguna
    <br />
-   <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2.5 me-2 mb-2 mt-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Tambah Data</button>
+   <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2.5 me-2 mt-5 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Tambah Data</button>
    </div>;
 };
 
