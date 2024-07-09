f92be62df573551dc20d0e9c1948c21606d70dc4
kulkad <rdarmawan281@gmail.com>
2024-07-09 10:10:48 +0700

    membuat halaman data user

--- app/datamagang/page.jsx
+++ app/datamagang/page.jsx
@@ -5,7 +5,7 @@ import { MdDelete } from "react-icons/md";
 const DataPengguna = () => {
   return (
     <div className="bg-white rounded-lg mx-4 p-4 text-xl">
-      <p className="mt-1 mb-4">DATA PENGGUNA</p>
+      <p className="mt-1 mb-4">DATA MAGANG</p>
 
       <div class="relative overflow-x-auto">
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
