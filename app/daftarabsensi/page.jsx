1e48d46d7769266a806f89c8aa04186d11f54bc0
ryunex15 <badarudin1511@gmail.com>
2024-07-09 10:40:33 +0700

    fix

--- app/daftarabsensi/page.jsx
+++ app/daftarabsensi/page.jsx
@@ -2,37 +2,37 @@ import React from "react";
 import { Dropdown } from "flowbite-react";
 
 const DaftarAbsensi = () => {
-   return <div className="bg-white rounded-lg p-4 text-xl">Daftar Absensi
+   return <div className="bg-white rounded-lg p-4 text-xl dark:bg-gray-600 dark:text-white">Daftar Absensi
    <hr class="mt-5 mb-2"/>
     
-   <div class="max-w-2xl mx-auto bg-white p-2 rounded-lg mb-2">
+   <div class="max-w-2xl mx-auto bg-white p-2 rounded-lg mb-2 dark:bg-gray-600">
    <form class="flex flex-wrap items-center space-x-4">
       
       <div>
-        <label for="role" class="block text-sm font-medium text-gray-700">Role</label>
-        <select id="role" name="role" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-          <option>Manager</option>
-          <option>Karyawan</option>
-          <option>Magang</option>
-          <option>PKL</option>
+        <label for="role" class="block text-sm font-medium text-gray-700 dark:text-gray-200">Role</label>
+        <select id="role" name="role" class="dark:text-gray-800 mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+          <option className="dark:text-gray-800">Manager</option>
+          <option className="dark:text-gray-800">Karyawan</option>
+          <option className="dark:text-gray-800">Magang</option>
+          <option className="dark:text-gray-800">PKL</option>
         </select>
       </div>
       
       <div>
-        <label for="name" class="block text-sm font-medium text-gray-700">Nama</label>
-        <select id="name" name="name" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+        <label for="name" class="block text-sm font-medium text-gray-700 dark:text-gray-200">Nama</label>
+        <select id="name" name="name" class="dark:text-gray-800 mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
           <option>John Doe</option>
           <option>Michael Johnson</option>
         </select>
       </div>
       
       <div>
-        <label for="date" class="block text-sm font-medium text-gray-700">Date</label>
-        <input type="date" id="date" name="date" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"></input>
+        <label for="date" class="block mt-1 text-sm font-medium text-gray-700 dark:text-gray-200">Date</label>
+        <input type="date" id="date" name="date" class="dark:text-gray-800 mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"></input>
       </div>
       
       <div class="self-end">
-        <button type="submit" class="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+        <button type="submit" class="mb-1 w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
           Search
         </button>
       </div>
@@ -42,7 +42,7 @@ const DaftarAbsensi = () => {
 
 <div class="relative overflow-x-auto">
     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-900 uppercase dark:text-gray-400">
+        <thead class="text-xs text-gray-900 uppercase dark:text-gray-200">
             <tr>
                 <th scope="col" class="px-6 py-3">
                     No
@@ -62,20 +62,20 @@ const DaftarAbsensi = () => {
             </tr>
         </thead>
         <tbody>
-            <tr class="bg-white dark:bg-gray-800">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            <tr class="bg-white dark:bg-gray-700">
+                <th scope="row" class="px-6 py-4 font-large text-gray-900 whitespace-nowrap dark:text-gray-200">
                     1
                 </th>
-                <td class="px-6 py-4">
+                <td class="px-6 py-4 dark:text-gray-200">
                     Jhon
                 </td>
-                <td class="px-6 py-4">
+                <td class="px-6 py-4 dark:text-gray-200">
                     PKL
                 </td>
-                <td class="px-6 py-4">
+                <td class="px-6 py-4 dark:text-gray-200">
                     07:53
                 </td>
-                <td class="px-6 py-4">
+                <td class="px-6 py-4 dark:text-gray-200">
                     Monday, 10 July 2024
                 </td>
             </tr>
